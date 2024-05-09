56e787ac835d503a67319be92edb0287b12070a2
Carl Fremault <carlfremault@yahoo.com>
2024-05-09 12:23:11 +0200

    setup database

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS markets;
+
+CREATE TABLE accounts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    account_name TEXT NOT NULL,
+    exchange_name TEXT NOT NULL,
+	public_key TEXT NOT NULL,
+	private_key TEXT NOT NULL
+);
+
+CREATE TABLE markets (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	market_name TEXT NOT NULL,
+	market_account INTEGER,
+	FOREIGN KEY (market_account) REFERENCES accounts(id)
+);
+	
